11a900463182aa06f214b54696331f65812693e9
itsvivekghosh <soapmactevis1@gmail.com>
2020-07-23 22:00:44 +0530

    Tree traversal Iteratively

--- Tree Traversal Iteratively/TreetraversalIteratively.cpp
+++ Tree Traversal Iteratively/TreetraversalIteratively.cpp
@@ -38,6 +38,59 @@ void printInorderUsingIteration(Node *root) {
     }
 }
 
+
+void printPreorderUsingRecursion(Node *root) {
+    if (root == NULL) return;
+
+    cout<<root->data<<" ";
+    printPreorderUsingRecursion(root->left);
+    printPreorderUsingRecursion(root->right);
+    return;
+}
+
+void printPreOrderUsingIteration(Node *root) {
+
+    unordered_map<Node*, int> cnt;
+    stack<Node *> S;
+    S.push(root);
+
+    while(!S.empty()) {
+        Node *curr = S.top();
+        if(curr == NULL) {S.pop(); continue;}
+        if(cnt[curr] == 0) cout<<curr->data<<" ";
+        else if(cnt[curr] == 1) S.push(curr->left);
+        else if(cnt[curr] == 2) S.push(curr->right);
+        else S.pop();
+        cnt[curr]++;
+    }
+}
+
+void printPostorderUsingRecursion(Node *root) {
+    if (root == NULL) return;
+
+    printPostorderUsingRecursion(root->left);
+    printPostorderUsingRecursion(root->right);
+    cout<<root->data<<" ";
+    return;
+}
+
+void printPostorderUsingIteration(Node *root) {
+
+    unordered_map<Node*, int> cnt;
+    stack<Node *> S;
+    S.push(root);
+
+    while(!S.empty()) {
+        Node *curr = S.top();
+        if(curr == NULL) {S.pop(); continue;}
+        if(cnt[curr] == 0) S.push(curr->left);
+        else if(cnt[curr] == 1) S.push(curr->right);
+        else if(cnt[curr] == 2) cout<<curr->data<<" ";
+        else S.pop();
+        cnt[curr]++;
+    }
+}
+
 int main(void) {
 
     Node *root = new Node(6); 
@@ -48,8 +101,19 @@ int main(void) {
     root->right->left = new Node(11);
     root->right->right = new Node(19);
 
-    cout<<"Printing using Recursion: "<<endl;
+    cout<<"Printing Inorder using Recursion: "<<endl;
     printInorderUsingRecursion(root);cout<<endl;
-    cout<<"Printing using Iteration: "<<endl;
+    cout<<"Printing Inorder using Iteration: "<<endl;
     printInorderUsingIteration(root); cout<<endl;
+
+
+    cout<<"Printing PreOrder using Recursion: "<<endl;
+    printPreorderUsingRecursion(root);cout<<endl;
+    cout<<"Printing PreOrder using Iteration: "<<endl;
+    printPreOrderUsingIteration(root); cout<<endl;
+
+    cout<<"Printing PostOrder using Recursion: "<<endl;
+    printPostorderUsingRecursion(root);cout<<endl;
+    cout<<"Printing PostOrder using Iteration: "<<endl;
+    printPostorderUsingIteration(root); cout<<endl;
 }
