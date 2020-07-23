3a287245f14d0307bd330a805a6cffa401c331da
itsvivekghosh <soapmactevis1@gmail.com>
2020-07-23 22:56:28 +0530

    Find Two Numbers from two trees that added equal to 'S'

---
+++ FindTwoNumberFromTwoTrees/FindNumbers.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+
+    public: 
+        int data;
+        Node *left, *right;
+        vector<int> arr1, arr2;
+
+        Node() {}
+        Node(int data) {
+            this->data=data;
+            this->left=this->right=NULL;
+        }
+
+        void processInorder(Node *root, vector<int> &v) {
+
+            if(root == NULL) return;
+            processInorder(root->left, v);
+            // cout<<root->data<<endl;
+            v.push_back(root->data);
+            processInorder(root->right, v);
+
+            return;
+        }
+
+        void findSolution(Node *root1, Node *root2) {
+            int s=30;
+            
+            processInorder(root1, arr1);
+            processInorder(root2, arr2);
+
+            // for(auto a: arr1) cout<<a<<" ";cout<<endl;
+            // for(auto a: arr2) cout<<a<<" ";
+
+            int i=0, j=arr2.size()-1;
+            while(i<arr1.size() and j>=0) {
+                if(arr1[i] + arr2[j] == s) {
+                    cout<<arr1[i]<<" "<<arr2[j]<<endl;
+                    break;
+                }
+                else if(arr1[i]+arr2[j] < s) {
+                    i++;
+                }
+                else {
+                    j--;
+                }
+            }
+        }
+};
+
+int main(void) {
+
+    Node *root = new Node(6); 
+    root->left = new Node(3); 
+    root->right = new Node(17); 
+    root->left->left = new Node(0); 
+    root->left->right = new Node(4);
+    root->right->left = new Node(11);
+    root->right->right = new Node(19);
+
+    Node *root2 = new Node(8); 
+    root2->left = new Node(5); 
+    root2->right = new Node(13); 
+    root2->left->left = new Node(1); 
+    root2->left->right = new Node(7);
+
+    Node *obj = new Node();
+    obj->findSolution(root, root2);
+}
