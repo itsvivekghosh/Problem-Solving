0dfcbb28d85987359ef478f3103b8eb0d7d2791e
itsvivekghosh <soapmactevis1@gmail.com>
2020-07-31 18:23:47 +0530

    Added Rain Water Trapping with O(n) space Complexity

--- Rain Water Trapping/RainWaterTapping.cpp
+++ Rain Water Trapping/RainWaterTapping.cpp
@@ -19,6 +19,30 @@ int findSolution1(vector<int> arr, int n) { // in O(n^2) Time Complexity
     return ans;
 }
 
+int findSolution2(vector<int> arr, int n) { // improvement Over O(N^2) algorithm (above solution) but using O(N) space Complexity
+    
+    vector<int> left(n), right(n);
+    int ans=0;
+    
+    // capturing the highest bar from left to right
+    left[0] = arr[0];
+    for(int i=1;i<n;++i) {
+        left[i] = max(left[i-1], arr[i]);
+    }
+    
+    // capturing the highest bar from right to left
+    right[n-1] = arr[n-1];
+    for(int i=n-2;i>=0;--i) {
+        right[i] = max(right[i+1], arr[i]);
+    }
+    
+    for(int i=0;i<n;++i) {
+        ans += (min(left[i], right[i]) - arr[i]);
+    }
+    
+    return ans;
+}
+
 int main() {
     int t; cin>>t;
     
@@ -28,5 +52,6 @@ int main() {
         for(auto &a: arr) cin>>a;
         
         cout<<findSolution1(arr, n)<<endl;
+        cout<<findSolution2(arr, n)<<endl;
     }
 }
